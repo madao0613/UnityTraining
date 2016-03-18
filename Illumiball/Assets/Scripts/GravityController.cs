4a34db8533acdde78e103c6da9996b975907f326
madao0613 <taketaka0613@gmail.com>
2016-03-18 22:18:53 +0900

    build test

--- Illumiball/Assets/Scripts/GravityController.cs
+++ Illumiball/Assets/Scripts/GravityController.cs
@@ -18,19 +18,29 @@ public class GravityController : MonoBehaviour {
 	void Update () {
         Vector3 vector = new Vector3();
 
-        //キーの入力を検知しベクトルを設定
-        vector.x = Input.GetAxis("Horizontal");
-        vector.z = Input.GetAxis("Vertical");
-        
-        //高さ方向の判定キーはZキーとする
-        if (Input.GetKey("z"))
+        if (Application.isEditor)
         {
+            //キーの入力を検知しベクトルを設定
+            vector.x = Input.GetAxis("Horizontal");
+            vector.z = Input.GetAxis("Vertical");
 
-            vector.y = 1.0f;
+            //高さ方向の判定キーはZキーとする
+            if (Input.GetKey("z"))
+            {
 
+                vector.y = 1.0f;
+
+            } else {
+
+                vector.y = -1.0f;
+
+            }
         } else {
 
-            vector.y = -1.0f;
+            //加速度センサーの入力をUnity空間の軸にマッピングする
+            vector.x = Input.acceleration.x;
+            vector.y = Input.acceleration.y;
+            vector.z = Input.acceleration.z;
 
         }
 
