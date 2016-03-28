845554657f87a617dd979937fb2521809b04c7d2
madao0613 <taketaka0613@gmail.com>
2016-03-28 22:54:13 +0900

    CandyDozer

---
+++ CandyDozer/Assets/Scripts/CandyDestroyer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CandyDestroyer : MonoBehaviour {
+    
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.tag == "Candy")
+        {
+            // オブジェクトの削除
+            Destroy(other.gameObject);
+        }
+    }
+
+}
