e9210b83d84dcb2b6b6df44bab6077807f6a6add
madao0613 <taketaka0613@gmail.com>
2016-04-09 00:32:29 +0900

    CandyDozer MasterUP

--- CandyDozer/Assets/Scripts/CandyDestroyer.cs
+++ CandyDozer/Assets/Scripts/CandyDestroyer.cs
@@ -2,13 +2,31 @@
 using System.Collections;
 
 public class CandyDestroyer : MonoBehaviour {
-    
+
+    public CandyHolder candyHolder;
+    public int reward;
+    public GameObject effectPrefab;
+    public Vector3 effectRotation;
+
     void OnTriggerEnter (Collider other)
     {
         if (other.gameObject.tag == "Candy")
         {
+            // 指定数だけCandyのストックを増やす
+            candyHolder.AddCandy(reward);
+
             // オブジェクトの削除
             Destroy(other.gameObject);
+
+            if (effectPrefab != null)
+            {
+                // Candyのポジションにエフェクトを生成
+                Instantiate(
+                    effectPrefab,
+                    other.transform.position,
+                    Quaternion.Euler(effectRotation)
+                );
+            }
         }
     }
 
--- CandyDozer/Assets/Scripts/Shooter.cs
+++ CandyDozer/Assets/Scripts/Shooter.cs
@@ -4,16 +4,26 @@ using System.Collections;
 public class Shooter : MonoBehaviour {
 
     const int SphereCandyFrequency = 3;
+    const int MaxShotPower = 5;
+    const int RecoverySeconds = 3;
 
     int sampleCandyCount;
+    int shotPower = MaxShotPower;
+    AudioSource shotSound;
+
     public GameObject[] candyPrefabs;
     public GameObject[] candySquarePrefabs;
-    public GameObject candyHolder;
+    public CandyHolder candyHolder;
 
     public GameObject candyPrefab;
     public float shotSpeed;
     public float shotTorque;
     public float baseWidth;
+
+    void Start()
+    {
+        shotSound = GetComponent<AudioSource>();
+    }
     	
 	// Update is called once per frame
 	void Update () {
@@ -53,6 +63,10 @@ public class Shooter : MonoBehaviour {
 
     public void Shot()
     {
+        // キャンディを生成できる条件外ならばshotしない
+        if (candyHolder.GetCandyAmount() <= 0) return;
+        if (shotPower <= 0) return;
+
         // プレファブからCandyオブジェクトを生成
         GameObject candy = (GameObject)Instantiate(
             SampleCandy(),
@@ -68,5 +82,39 @@ public class Shooter : MonoBehaviour {
         candyRigidBody.AddForce(transform.forward * shotSpeed);
         candyRigidBody.AddTorque(new Vector3(0, shotTorque, 0));
 
+        // キャンディストックを消費
+        candyHolder.ConsumeCandy();
+        // ShotPowerを消費
+        ConsumePower();
+
+        // サウンドを再生
+        shotSound.Play();
+
+    }
+
+    void OnGUI()
+    {
+        GUI.color = Color.black;
+
+        // ShotPower の残数を + の数で表示
+        string label = "";
+        for (int i = 0; i < shotPower; i++) label = label + "+";
+
+        GUI.Label(new Rect(0, 15, 100, 30), label);
+
+    }
+
+    void ConsumePower()
+    {
+        // ShotPower を消費すると同時に回復のカウントをスタート
+        shotPower--;
+        StartCoroutine(RecoveryPower());
+    }
+
+    IEnumerator RecoveryPower()
+    {
+        // 一定秒数待った後にshotPowerを回復
+        yield return new WaitForSeconds(RecoverySeconds);
+        shotPower++;
     }
 }
